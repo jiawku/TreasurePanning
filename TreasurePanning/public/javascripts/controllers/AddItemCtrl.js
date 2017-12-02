f8c7445d99a30a235152bcb95f9d204488a71a25
jiawku <jiawku@hotmail.com>
2017-12-01 19:51:30 -0600

    update login function, still need improve

--- TreasurePanning/public/javascripts/controllers/AddItemCtrl.js
+++ TreasurePanning/public/javascripts/controllers/AddItemCtrl.js
@@ -8,9 +8,7 @@ app.controller('AddItemCtrl', ['$timeout','$scope','multipartForm','$location',
         $scope.successMessgae='Item is added successfully.';
         $scope.item={};
         $scope.showForm=undefined;
-        console.log("getinto timer");
         $timeout(function () {
-          console.log("timeout");
           $location.path("/");
         }, 2000);
       });
--- TreasurePanning/routes/index.js
+++ TreasurePanning/routes/index.js
@@ -13,38 +13,32 @@ var router=express.Router();
     router.get('/register', function(req, res) {
         res.render('register', { });
     });
-    /*
+
     router.post('/register', function(req, res) {
       Account.register(new Account({ username : req.body.username , firstname: req.body.firstname,lastname:req.body.lastname,phone: Math.round(req.body.phone),address:req.body.address,emailaddress: req.body.emailaddress,isDeleted:'False',isAdmin:'False'}), req.body.password, function(err, account) {
           if (err) {
-              res.end("error");
+              return res.status(500).json({err:err});
           }
 
           passport.authenticate('local')(req, res, function () {
-            res.redirect('/');
+              return res.status(200).json({
+                status:"Registration successful!"
+              })
           });
       });
     });
-    */
-    router.post('/register', function(req, res) {
-      Account.register(new Account({ username : req.body.username , firstname: req.body.firstname,lastname:req.body.lastname,phone: Math.round(req.body.phone),address:req.body.address,emailaddress: req.body.emailaddress,isDeleted:'False',isAdmin:'False'}), req.body.password, function(err, account) {
-          if (err) {
-              return res.render('/register', { account : account });
 
-          }
-
-          passport.authenticate('local')(req, res, function () {
-            res.redirect('/');
-          });
-      });
-    });
     router.get('/login', function(req, res) {
         res.render('login', { user : req.user });
     });
 
     router.post('/login', passport.authenticate('local'), function(req, res) {
+
+        // res.end('loginSuccess');
         res.redirect('/');
     });
+
+
 
     router.get('/logout', function(req, res) {
         req.logout();
